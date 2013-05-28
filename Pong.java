304612338337bf2fafb3c9dbdd35f3f7c1f7d48a
metroxel <megan.troxel.12@cnu.edu>
2013-05-28 14:25:23 -0400

    added images
    - added player1 and player2 paddles

--- Pong.java
+++ Pong.java
@@ -15,7 +15,6 @@ import javax.swing.JPanel;
 public class Pong extends JFrame {
 	private static JFrame pong = new JFrame("Pong Game");
 	
-	
 	/*
 	 * Default Constructor that creates the JPanel which the is put on the JFrame
 	 * allows the paddles and ball to have something to be built on 
@@ -24,14 +23,22 @@ public class Pong extends JFrame {
 		JPanel game	= new JPanel();
 		game.setLayout(new FlowLayout());
 	
-		
-		// reads image and adds a label to the panel
+		// reads pongball image and adds a label to the panel
 		JLabel pongBallLabel = new JLabel(new ImageIcon("Pongball.png"));
 		game.add(pongBallLabel);
 		// adds label to the Frame
 		this.add(game);
+		
+		// reads player1 and player2 paddles and adds a label to the panel
+		JLabel player1Label = new JLabel(new ImageIcon("player1.png"));
+		game.add(player1Label);
+		this.add(game);
+		
+		JLabel player2Label =  new JLabel(new ImageIcon("player2.png"));
+		game.add(player2Label);
+		this.add(game);
+		
 	}
-	
 	
 	/*
 	 * creates the frame that the panel is created on
@@ -42,7 +49,6 @@ public class Pong extends JFrame {
 		frame.setTitle("Pong Game");
 		frame.setSize(600,400);	
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		frame.setVisible(true);
-	
+		frame.setVisible(true);	
 	}
 }
